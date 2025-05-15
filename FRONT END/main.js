77ca8709564496d65144056a163f9fdb937761c1
stef4nyy <stefany.lederme.goncalves@escola.pr.gov.br>
2025-05-15 11:20:47 -0300

    Update main.js

--- FRONT END/main.js
+++ FRONT END/main.js
@@ -16,3 +16,27 @@ document.addEventListener('DOMContentLoaded', function(){
     });
     });
     });
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+const hamburguer = document.getElementById('hamburguer');
+const navMenu = document.getElementById ('navMenu');
+
+hamburguer.addEventListener('click', function() {
+hamburguer.classList.toggle('active');
+navMenu.classList.toggle('active');
+
+});
+
+const navItems = document.querySelectorAll('.nav-item');
+navItems.forEach(item => {
+item.addEventListener('click', function(){
+hamburguer.classList.remove('active');
+navMenu.classList.remove('active');
+});
+});
+});
